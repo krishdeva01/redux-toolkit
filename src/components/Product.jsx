9884897f217297be3ff0ce14240bc428544f3320
krishdeva01 <kdevaraj@freewheel.tv>
2024-07-13 23:34:14 +0530

    implemented useReducer hook

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+const transactionReducer = (state, action) => {
+  switch (action.type) {
+    case 'WITHDRAW':
+      return state - action.payload
+    case 'DEPOSIT':
+      return state + action.payload
+    default: return state
+  }
+}
 
 const Product = () => {
+
+  const withdraw = (amount) => {
+    dispatch({type: 'WITHDRAW', payload: amount})
+  };
+
+  const deposit = (amount) => {
+    dispatch({type: 'DEPOSIT', payload: amount})
+  };
+
+  const [state, dispatch] = useReducer(transactionReducer, 1000);
   return (
     <div>
-      <h2>Product DashBoard</h2>
+{state}
+<button  onClick={()=>{withdraw(500)}}>withdraw</button>
+<button onClick={() =>{deposit(500)}} >deposit</button>
     </div>
   )
 }
